1d43aeb969d0d3dbc1164cae10d6cac680a7c2fb
Matt Brockman <mattbrockman@gmail.com>
2023-03-05 10:38:43 -0800

    add some stylign for the main app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import styled from "@emotion/styled";
 import { UserComponent } from "./components/user/UserComponent";
 import { WorldComponent } from "./components/world/WorldComponent";
 
 function App() {
   return (
-    <div className="App">
-      <UserComponent />
-      <WorldComponent />
-    </div>
+    <MainPage>
+      <GameDisplayRow>
+        <GameDisplayPanel>
+          <UserComponent />
+        </GameDisplayPanel>
+        <GameDisplayPanel>
+          <WorldComponent />
+        </GameDisplayPanel>
+      </GameDisplayRow>
+    </MainPage>
   );
 }
 
 export default App;
+
+const MainPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+  //tan background
+  background-color: #f5f5dc;
+  height: calc(100vh - 10px);
+  width: calc(100vw - 10px);
+  overflow: hidden;
+`;
+
+const GameDisplayRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: calc(100% - 30px);
+  height: calc(100% - 30px);
+  gap: 5px;
+`;
+
+const GameDisplayPanel = styled.div`
+  //this should take up half the width of the parent, which is the GameDisplayRow
+  padding: 5px;
+  height: 100%;
+  width: 50%;
+  //brown border shading
+  border: 1px solid #8b4513;
+`;
